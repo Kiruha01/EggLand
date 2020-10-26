62d78a97b8e3e9ad8dbf9a2ba32c599dae8495a6
Kiruha01 <Kiruha01@users.noreply.github.com>
2020-10-26 22:08:09 +0300

    Working! But ugly interface

--- app/src/main/java/com/Kirilllis/TimersAdapter.kt
+++ app/src/main/java/com/Kirilllis/TimersAdapter.kt
@@ -10,21 +10,42 @@ import android.widget.ImageView
 import android.widget.TextView
 import kotlin.concurrent.timer
 
+
 class TimersAdapter(var timers: Array<TimerTile>, val context: Context): BaseAdapter(){
 
-    var lInflater = LayoutInflater.from(context)
+    private val lInflater = LayoutInflater.from(context)
+
+    private class ViewHolder(view: View){
+        val nameTextView = view.findViewById<TextView>(R.id.nameCard)
+        val timeTextView = view.findViewById<TextView>(R.id.timeCard)
+        val pictureImageView = view.findViewById<ImageView>(R.id.icon)
+    }
+
     override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
-        var view = convertView
+
+        lateinit var viewHolder: ViewHolder
+        lateinit var view: View
         if (convertView == null){
-            view = lInflater?.inflate(R.layout.timer_item, viewGroup, false)
-            view?.findViewById<TextView>(R.id.nameCard)?.setText(position.toString())
-            view?.findViewById<TextView>(R.id.timeCard)?.setText("${if (timers[position].getRemainingSeconds()/60 < 10) "0" else ""}${timers[position].getRemainingSeconds()/60}:${if (timers[position].getRemainingSeconds() % 60 < 10) "0" else ""}${timers[position].getRemainingSeconds()%60}")
-            view?.findViewById<ImageView>(R.id.icon)?.setImageResource(timers[position].getImageId())
+            view = lInflater.inflate(R.layout.timer_item, viewGroup, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+
+
+//            view?.findViewById<TextView>(R.id.nameCard)?.setText(position.toString())
+//            view?.findViewById<TextView>(R.id.timeCard)?.setText("${if (timers[position].getRemainingSeconds()/60 < 10) "0" else ""}${timers[position].getRemainingSeconds()/60}:${if (timers[position].getRemainingSeconds() % 60 < 10) "0" else ""}${timers[position].getRemainingSeconds()%60}")
+//            view?.findViewById<ImageView>(R.id.icon)?.setImageResource(timers[position].getImageId())
         }
         else{
-            view?.findViewById<TextView>(R.id.timeCard)?.setText("${if (timers[position].getRemainingSeconds() / 60 < 10) "0" else ""}${timers[position].getRemainingSeconds() / 60}:${if (timers[position].getRemainingSeconds() % 60 < 10) "0" else ""}${timers[position].getRemainingSeconds() % 60}")
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+//            view?.findViewById<TextView>(R.id.timeCard)?.setText("${if (timers[position].getRemainingSeconds() / 60 < 10) "0" else ""}${timers[position].getRemainingSeconds() / 60}:${if (timers[position].getRemainingSeconds() % 60 < 10) "0" else ""}${timers[position].getRemainingSeconds() % 60}")
         }
-        return view!!;
+        Log.d("DEBUG", "Callig with " + position.toString())
+        viewHolder.timeTextView.setText(timers[position].getRemainingSeconds().toString())
+        viewHolder.nameTextView.setText(position.toString())
+        viewHolder.pictureImageView.setImageResource(timers[position].getImageId())
+        //viewHolder.timeTextView.setText("${if (timers[position].getRemainingSeconds() / 60 < 10) "0" else ""}${timers[position].getRemainingSeconds() / 60}:${if (timers[position].getRemainingSeconds() % 60 < 10) "0" else ""}${timers[position].getRemainingSeconds() % 60}")
+        return view
     }
 
     override fun getItem(p0: Int): Any {
